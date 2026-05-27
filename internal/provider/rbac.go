4c39c04a5ab385d975dd6984b4e88b2c19c4afce
Chi Fujii <chi.fujii@solanica.io>
2026-05-27 15:38:06 +0900

    allow creating new instance from backup
    Signed-off-by: Chi Fujii <chi.fujii@solanica.io>

--- internal/provider/rbac.go
+++ internal/provider/rbac.go
@@ -44,7 +44,7 @@ package provider
 // +kubebuilder:rbac:groups=backup.openeverest.io,resources=backups/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=backup.openeverest.io,resources=backupclasses,verbs=get;list;watch
 // +kubebuilder:rbac:groups=backup.openeverest.io,resources=backupstorages,verbs=get;list;watch
-// +kubebuilder:rbac:groups=backup.openeverest.io,resources=restores,verbs=get;list;watch;update;patch
+// +kubebuilder:rbac:groups=backup.openeverest.io,resources=restores,verbs=get;list;watch;create;update;patch
 // +kubebuilder:rbac:groups=backup.openeverest.io,resources=restores/status,verbs=get;update;patch
 
 // =============================================================================
